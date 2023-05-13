d124a2af6252b318d9e056fe501ab0661cef4b91
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-05-13 10:51:10 +0200

    Adjusted User component.

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -5,7 +5,12 @@ const User = ( { user: { name, mail, gender } } ) => {
         <ul>
             <li>Name: {name}</li>
             <li>Mail: {mail}</li>
-            <li>Gender: {gender}</li>
+            <li>Gender: 
+                {
+                    //Sets the first letter to uppercase in the gender attribute, since it's all lowercase by default.
+                    " " + gender.charAt(0).toUpperCase() + gender.slice(1)
+                }
+            </li>
         </ul>
     );
 }
