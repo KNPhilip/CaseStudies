039aca093d8405cc57de97bdd9ff17fd04229187
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-05-13 11:12:16 +0200

    SCSS adjustments

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const User = ( { user: { name, mail, gender } } ) => {
+const User = ( { user: { name, email, gender } } ) => {
     return (
         <ul>
             <li>Name: {name}</li>
-            <li>Mail: {mail}</li>
+            <li>Mail: {email}</li>
             <li>Gender: 
                 {
                     //Sets the first letter to uppercase in the gender attribute, since it's all lowercase by default.
