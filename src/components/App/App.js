e3e66f40c8b2d8f41090156bc6644dfd3ea71ed9
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-05-13 10:56:38 +0200

    Created input & button in the UI

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,29 @@
+import React, { useState } from 'react';
 import './App.css';
+import { Typography, TextField, Button } from '@mui/material';
+import User from '../User/User';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Typography variant='h2'>Lookup App</Typography>
+
+      <div className='search'>
+        <TextField 
+          id="input"
+          label="Enter name"
+          variant="outlined"
+        />
+
+        <Button
+          variant='contained'
+        >Search</Button>
+      </div>
+
+      {
+        
+      }
+    </>
   );
 }
 
