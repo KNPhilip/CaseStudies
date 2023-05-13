6d01da60312ea93c0254234117d86ff58695feec
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-05-13 10:44:22 +0200

    Import of MUI and created custom theme

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
+//Imports the prerequisites to make custom themes.
+import { ThemeProvider, createTheme } from '@mui/material';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
+//Creates a new theme called lookupapp and removing border radius on it.
+const lookupapp = createTheme({
+  shape: {
+    borderRadius: "0px"
+  }
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={lookupapp}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
