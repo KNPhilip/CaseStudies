13e4fd9259d31bc5d3301b34f23ea9b93db71ff7
Philip Krag Nielsen <phil5520@edu.campusvejle.dk>
2023-05-13 11:04:02 +0200

    Finished UI and added states

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,14 @@ import { Typography, TextField, Button } from '@mui/material';
 import User from '../User/User';
 
 function App() {
+
+  //useState for the current Search Term. Used to find the results from the API later.
+  const [searchTerm, setSearchTerm] = useState('');
+  //Another useState for the users (results) from the API.
+  const [users, setUsers] = useState([]);
+  //Is true or false depending on the length of the current search. (This is used to enable/disable searching)
+  const isDisabled = searchTerm.length <= 3;
+
   return (
     <>
       <Typography variant='h2'>Lookup App</Typography>
@@ -13,15 +21,35 @@ function App() {
           id="input"
           label="Enter name"
           variant="outlined"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
 
         <Button
           variant='contained'
+          disabled={isDisabled}
         >Search</Button>
       </div>
 
       {
-        
+        users?.length > 0 
+        ? (
+          <div>
+            <Typography variant='p'>Results</Typography>
+            {
+              //If results is greater than 0, map through all the users and display them (This happens through the User component)
+              users.map((user) => (
+                <User user={user} key={user.id} />
+              ))
+            }
+          </div>
+
+        ) : (
+          //Displays if there was no found users.
+          <div>
+            <Typography variant='p'>No users found..</Typography>
+          </div>
+        )
       }
     </>
   );
